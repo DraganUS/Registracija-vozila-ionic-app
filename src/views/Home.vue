d830b11c24edfcac62ad98be46f598c0cbb28b49
DraganUS <usic.d.dragan@gmail.com>
2019-06-26 18:16:38 +0200

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,18 +7,9 @@
     </ion-header>
     <ion-content class="ion-padding">
       
-        <ion-item>
-          <ion-label position="floating">Unesite Vaše</ion-label>
-          <ion-input type="text" placeholder="Ime"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label position="floating">Unesite Vaše Prezime</ion-label>
-           <ion-input type="text" placeholder="Prezime"></ion-input>
-        </ion-item>
-
    <ion-item>
       <ion-label>Izaberite opštinu:<span>*</span></ion-label>
-      <ion-select value="Putničko" okText="Okay" cancelText="Dismiss" required>
+      <ion-select value="Putničko"  okText="Okay" cancelText="Dismiss" required>
          <ion-select-option value="1" > Barajevo</ion-select-option>
          <ion-select-option value="2" >Čukarica</ion-select-option>
          <ion-select-option value="3" >Grocka</ion-select-option>
@@ -37,15 +28,20 @@
          </ion-select>
      </ion-item>
 
-     <ion-item>
+     <ion-item >
       <ion-label>Vrsta vozila:<span>*</span></ion-label>
-      <ion-select value="Putničko" okText="Okay" cancelText="Dismiss">
-        <ion-select-option value="brown">Putničko</ion-select-option>
-        <ion-select-option value="blonde">Teretno</ion-select-option>
-        <ion-select-option value="black">Motocikli</ion-select-option>
-        <ion-select-option value="red">Priklkucno</ion-select-option>
+      <ion-select @ionChange='tipVozila=event.target.value' :value="tipVozila" okText="Okay" cancelText="Dismiss">
+        <ion-select-option @ionChange='tipVozila=event.target.value' @click="promenaTipVozila()"  value="putnicko">Putničko</ion-select-option>
+        <ion-select-option @ionChange='tipVozila=event.target.value' value="teretno">Teretno</ion-select-option>
+        <ion-select-option @ionChange='tipVozila=event.target.value' value="motocikli">Motocikli</ion-select-option>
       </ion-select>
     </ion-item>   
+    
+      <ion-item v-if="tipVozila=='teretno'">
+        <ion-label position="floating">Tezina <span>*</span></ion-label>
+        <ion-input type="number" description="numeric"></ion-input>
+      </ion-item>
+    <!-- Ako se izabere teretno vozilo doda se nova kolona nosivost -->
 
     <ion-item>
     <ion-label>Godište: <span>*</span></ion-label>
@@ -53,8 +49,8 @@
     </ion-item>
     
      <ion-item>
-        <ion-label position="floating">Snaga motora u KW: <span>*</span></ion-label>
-        <ion-input type="number" description="numeric"></ion-input>
+        <ion-label  position="floating">Snaga motora u KW: <span>*</span></ion-label>
+        <ion-input  type="number" description="numeric"></ion-input>
       </ion-item>
 
         <ion-item>
@@ -62,87 +58,90 @@
         <ion-input type="number" description="numeric"></ion-input>
       </ion-item>
 
-    <ion-item >
-      <ion-label>Ostali podaci:</ion-label>
-      <ion-select  multiple="true" cancelText="Cancel" okText="Ok">
-        <ion-select-option  value="1">Vodi na firmu</ion-select-option>
-        <ion-select-option value="2">Kupljeno na lizing</ion-select-option>
-        <ion-select-option value="3">Vozilo je TAXI</ion-select-option>
-        <ion-select-option value="4">TAXI ima atest plinski uredaj</ion-select-option>
-        <ion-select-option value="5">Vlasnik invalid preko 80%</ion-select-option>
-        <ion-select-option value="5">Vlasništvo rent-a-car</ion-select-option>
-      </ion-select>
-    </ion-item>
-
-    
-
     <ion-item>
-      <ion-label>Saobracajne i tablice:</ion-label>
-      <ion-select multiple="true" cancelText="Cancel" okText="Ok">
-        <ion-select-option value="1"> + nova saobraćajna</ion-select-option>
-        <ion-select-option value="2"> + tablice </ion-select-option>
-      </ion-select >
+      <ion-label> Premijski stepen:<span>*</span></ion-label>
+      <ion-select value="" okText="Okay" cancelText="Dismiss">
+        <ion-select-option value="newCar">Tek nabvljeno vozilo</ion-select-option>
+        <ion-select-option value="1">1</ion-select-option>
+        <ion-select-option value="2">2</ion-select-option>
+        <ion-select-option value="3">3</ion-select-option>
+        <ion-select-option value="4">4</ion-select-option>
+        <ion-select-option value="5">5</ion-select-option>
+        <ion-select-option value="6">6</ion-select-option>
+        <ion-select-option value="7">7</ion-select-option>
+        <ion-select-option value="8">8</ion-select-option>
+        <ion-select-option value="9">9</ion-select-option>
+        <ion-select-option value="10">10</ion-select-option>
+        <ion-select-option value="11">11</ion-select-option>
+        <ion-select-option value="12">12</ion-select-option>
+      </ion-select>
+    </ion-item> 
+
+      <ion-item>
+      <ion-label> Koliko ste šteta imali:<span>*</span></ion-label>
+      <ion-select value="" okText="Okay" cancelText="Dismiss">
+        <ion-select-option value="0">0</ion-select-option>
+        <ion-select-option value="1">1</ion-select-option>
+        <ion-select-option value="2">2</ion-select-option>
+        <ion-select-option value="3">3</ion-select-option>
+        <ion-select-option value="4">4</ion-select-option>
+        <ion-select-option value="5">5</ion-select-option>
+        <ion-select-option value="6">6</ion-select-option>
+        <ion-select-option value="7">7</ion-select-option>
+        <ion-select-option value="8">8</ion-select-option>
+        <ion-select-option value="9">9</ion-select-option>
+        <ion-select-option value="10">10</ion-select-option>
+        <ion-select-option value="11">11</ion-select-option>
+        <ion-select-option value="12">12</ion-select-option>
+      </ion-select>
+    </ion-item> 
+
+   <ion-list>  
+   <div id="saobracajna">
+    <ion-item> 
+      <ion-label>Vozilo se vodi na firmu</ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
     </ion-item>
- 
-
-
-
-<div mbsc-form>
-    <div class="mbsc-grid mbsc-form-grid">
-        <div class="mbsc-row">
-            <div class="mbsc-col-sm-12 mbsc-col-md-6">
-                <label>
-                    Mobile
-                    <select name="City" id="demo-multiple-lines" data-input-style="box" data-label-style="stacked">
-                        <option value="1">Percentage of assets covered by systematic risk assessments (>99)</option>
-                        <option value="2">Number of personal vacancies in the security roles required for ISMS (Nil)</option>
-                        <option value="3">Time taken to grant, change and remove access privileges (Max. 2 hours)</option>
-                        <option value="4">Percentage of agents covered by an effective security awareness program (100%)</option>
-                        <option value="5">Number of security access violations (<2)</option>
-                        <option value="6">Number of emergency changes (<5)</option>
-                        <option value="7">Number of security incidents involving malicious code (Max. 2)</option>
-                        <option value="8">Number of systems where security requirements are not met (Max. 2)</option>
-                        <option value="9">Average turnaround time of incidents (Max. 2 hours)</option>
-                        <option value="10">Number of pending actions to meet response and recovery requirements (Max. 5)</option>
-                        <option value="11">Number of scheduled internal audits not done (Max. 1)</option>
-                        <option value="12">Number of scheduled penetration tests not done (Max. 1)</option>
-                        <option value="13">Number of overdue actions arising out of audit reports (Max. 5)</option>
-                        <option value="14">Number of changes not carried out as per change control procedure (Max. 1)</option>
-                    </select>
-                </label>
-            </div>
-            <div class="mbsc-col-sm-12 mbsc-col-md-6">
-                <label>
-                    Desktop
-                    <select name="City" id="demo-multiple-lines-desktop" data-input-style="box" data-label-style="stacked">
-                        <option value="1">Percentage of assets covered by systematic risk assessments (>99)</option>
-                        <option value="2">Number of personal vacancies in the security roles required for ISMS (Nil)</option>
-                        <option value="3">Time taken to grant, change and remove access privileges (Max. 2 hours)</option>
-                        <option value="4">Percentage of agents covered by an effective security awareness program (100%)</option>
-                        <option value="5">Number of security access violations (<2)</option>
-                        <option value="6">Number of emergency changes (<5)</option>
-                        <option value="7">Number of security incidents involving malicious code (Max. 2)</option>
-                        <option value="8">Number of systems where security requirements are not met (Max. 2)</option>
-                        <option value="9">Average turnaround time of incidents (Max. 2 hours)</option>
-                        <option value="10">Number of pending actions to meet response and recovery requirements (Max. 5)</option>
-                        <option value="11">Number of scheduled internal audits not done (Max. 1)</option>
-                        <option value="12">Number of scheduled penetration tests not done (Max. 1)</option>
-                        <option value="13">Number of overdue actions arising out of audit reports (Max. 5)</option>
-                        <option value="14">Number of changes not carried out as per change control procedure (Max. 1)</option>
-                    </select>
-                </label>
-            </div>
-        </div>
+    <ion-item> 
+      <ion-label>Vozilo kupljeno na lizing</ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+      <ion-item> 
+      <ion-label> Vozilo je TAXI</ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+      <ion-item> 
+      <ion-label>TAXI vozilo ima atestiran plinski uredaj</ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+    <ion-item> 
+      <ion-label>Vlasnik vozila invalid preko 80%</ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+    <ion-item> 
+      <ion-label>Vozilo u vlasništvu rent-a-car firme </ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
     </div>
-</div>
+ </ion-list>
 
 
-     <ion-col class="ion-margin-top">
-      <div>
+ <ion-list>
+   <div id="saobracajna">
+    <ion-item> 
+      <ion-label>Potrebna mi je nova saobraćajna </ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+      <ion-item> 
+      <ion-label>Potrebne su mi tablice  </ion-label>
+      <ion-checkbox color="dark"></ion-checkbox>
+    </ion-item>
+    </div>
+ </ion-list>
+
+     <ion-col class="ion-margin-top">  
         <ion-button expand="block" color="dark" fill="outline" >Izračunaj</ion-button>
-      </div>
      </ion-col>
-       <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     </ion-content>
   </div>
 </template>
@@ -160,25 +159,13 @@ ion-toolbar{
   width: 100% !important;
   max-width: unset !important;
 }
-
-.alert-ios .alert-radio-label {white-space: pre-line !important;}
-.alert-md .alert-radio-label  {white-space: pre-line !important;}
-.alert-wp .alert-radio-label  {white-space: pre-line !important;}
-.select-text {white-space: pre-line !important;}
-/* .ios {
-  --ion-background-color: #222;
-  color: #fffffe;
+#saobracajna{
+  margin-top: 10px;
+  border: 1px solid black;
 }
-.ios ion-toolbar{
-  color: #222;
-}
-.ios ion-button{
-  color: #fffffe;
-} */
 .ion-margin-top{
   margin-top: 16px;
 }
-/* #fafafa */
 </style>
 
 
@@ -186,8 +173,17 @@ ion-toolbar{
 
 export default {
   name: "home",
-   data: {
-    opstina: 1
+   data() {
+     return{
+        opstina: 1,
+        tipVozila: '',
+        promenaTipVozila: ''
+     }
+    },
+     methods: {
+       promenaTipVozila: function(){
+        this.promenaTipVozila='sasd';
+     }
   },
 };
 </script>
